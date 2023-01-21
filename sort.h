aa1c8daed37b15cfdaf0d32f823c9ff82ea70abb
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-21 11:40:16 -0800

    Adding advanced task 7 files and updated sort.h

--- sort.h
+++ sort.h
@@ -26,5 +26,9 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
 
 #endif
